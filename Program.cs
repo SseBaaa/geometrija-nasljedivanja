0c4305da48543e3967191609dbcf8cb114c36411
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-03-22 13:15:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geomatrijsko_tijelo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kugla x = new Kugla();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kvadar y = new Kvadar();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+           
+
+            Console.ReadKey();
+        }
+    }
+
+    class GeometrijskoTijelo
+    {
+        protected double Volumen, Oplosje;
+        public static int BrojacTijela=0;
+
+        public GeometrijskoTijelo()
+        {
+            BrojacTijela++;
+        }
+    }
+
+    class Kugla : GeometrijskoTijelo
+    {
+        
+
+    }
+
+    class Kvadar : GeometrijskoTijelo
+    {
+       
+    }
+
+    class Kocka : Kvadar
+    {
+        
+    }
+}
